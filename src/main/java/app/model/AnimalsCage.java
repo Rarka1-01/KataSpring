6ac5ed25d7c8d8f245447b78d6a5e85d1b8dd467
Rarka <90555389+Rarka1-01@users.noreply.github.com>
2024-03-15 08:50:08 +0300

    Complite v1.0

---
+++ src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,27 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnimalsCage {
+
+    @Autowired
+    private @Qualifier("Dog") Animal animal;
+
+    @Autowired
+    private @Qualifier("Timer") Timer timer;
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }
+}
