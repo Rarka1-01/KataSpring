4c7488f96716409af1f352fba99e682126e252f4
Rarka <90555389+Rarka1-01@users.noreply.github.com>
2024-03-27 07:51:39 +0300

    Complite v2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,5 +1,7 @@
 package web.controller;
 
+import dao.CarDaoImp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +14,16 @@ import java.util.List;
 
 @Controller
 public class CarController {
+    private CarService carService;
+
+    @Autowired
+    public void setCarService(CarService carService) {
+        this.carService = carService;
+    }
+
     @GetMapping(value = "/cars")
     public String printListCars(@RequestParam(value = "count", defaultValue = "3") int count, ModelMap model) {
-        CarService carService = new CarServiceImp();
+
         List<Car> cars = carService.getCars(count);
 
         model.addAttribute("cars", cars);
